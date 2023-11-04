920e1686916d0e42f89f81a24df705ee1a00bcdd
Thuận Phát <phat06112003@gmail.com>
2023-11-04 15:31:32 +0700

    code import/save data

---
+++ src/main/java/Project.java
@@ -0,0 +1,94 @@
+import java.io.*;
+import java.util.*;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+class SlangUI extends JPanel {
+    public SlangUI() {
+        setLayout(new BorderLayout());
+    }
+    public static void createAndShowGUI() {
+        JFrame.setDefaultLookAndFeelDecorated(true);
+
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JComponent newContentPane = new SlangUI();
+
+        newContentPane.setOpaque(true);
+        frame.setContentPane(newContentPane);
+        frame.pack();
+        frame.setVisible(true);
+    }
+}
+
+class SlangFunction {
+    ArrayList<String[]> data = new ArrayList<String[]>();
+    String filePath;
+    public SlangFunction(String filePath) {
+        BufferedReader br;
+        String[] columnNames, student;
+        String line;
+
+        this.filePath = filePath;
+        try {
+            int currentIndex = 0;
+            String twoMeaningSlang;
+            br = new BufferedReader(new FileReader(filePath));
+            //remove the first line - instruction line(Slag`Meaning)
+            br.readLine().split("`");
+
+            while ((line = br.readLine()) != null) {
+                data.add(line.split("`"));
+                currentIndex++;
+            }
+            br.close();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    void save() {
+        BufferedWriter bw;
+        try {
+            String line;
+            bw = new BufferedWriter(new FileWriter(filePath));
+
+            bw.write("Slag`Meaning");
+            bw.newLine();
+            for (int i = 0; i < data.size(); i++) {
+                line = String.join("`", data.get(i));
+                bw.write(line);
+                if (i != data.size() - 1) {
+                    bw.newLine();
+                }
+            }
+            bw.flush();
+            bw.close();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    void print() {
+        for (int i = 0; i< data.size(); i++)
+            System.out.println(data.get(i)[0] + " : " + data.get(i)[1]);
+    }
+}
+
+public class Project {
+    static final String filePath = "C:\\LTUD Java\\Project01\\slang.txt";
+    public static void main(String[] args) {
+//        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+//            public void run() {
+//                SlangUI.createAndShowGUI();
+//            }
+//        });
+
+        SlangFunction sf = new SlangFunction(filePath);
+//        sf.print();
+    }
+}
